873049fc6881d23d1d847107a27957c09ce510cd
aviad sc <aviadsc@example.com>
2023-04-16 21:50:52 +0300

    16.4.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider:null,
+    web3:null,
+    contract:null
+  })
   return (
     <div className="App">
-      hello
+      <button
+      onClick={async () => {
+        const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
+        console.log(accounts)
+      } }
+      > connect to metamsk </button> 
     </div>
   );
 }
